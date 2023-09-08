c2e449fe2c80b5ace556f338e73cd3dcf0d6f459
Gajanan Deshmukh <gajanandeshmukh6260@gmail.com>
2023-09-08 17:26:40 +0530

    new project

---
+++ src/test/java/com/happiest/assignment/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happiest.assignment.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
