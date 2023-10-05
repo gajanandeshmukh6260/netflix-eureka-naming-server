50f8e6a8c0a21e0541496f19c40e806ef3cc22d8
Gajanan Deshmukh <gajanandeshmukh6260@gmail.com>
2023-10-05 14:27:17 +0530

    updated changes

--- src/test/java/com/happiest/assignment/NetflixEurekaNamingServerApplicationTests.java
+++ src/test/java/com/happiest/assignment/NetflixEurekaNamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.happiest.assignment.netflixeurekanamingserver;
+package com.happiest.assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
